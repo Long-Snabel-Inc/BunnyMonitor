a8e3616cc4d396f582dcf9a2fdc5ba8423a12bbb
Kristoffer Strube <kristoffer.strube@gmail.com>
2022-03-19 01:09:59 +0100

    Request for Location

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BunnyMonitor;
+using BlazorGeolocation;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<BlazorGeolocationService>();
 
 await builder.Build().RunAsync();
